f1a46212d3f30f280250cc394e4104ca74c5ebc2
Lean <109572399+LeandrOwO@users.noreply.github.com>
2023-12-22 22:18:08 -0300

    added rust-tools

--- init.lua
+++ init.lua
@@ -240,7 +240,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -617,6 +617,18 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+ local rt = require("rust-tools")
+ rt.setup({
+   server = {
+     on_attach = function(_, bufnr)
+     vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+     vim.keymap.set("n", "<leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr
+  })
+   end,
+   capabilities = capabilities
+   }
+ })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/plugins/rust-tools.lua
@@ -0,0 +1,41 @@
+return {
+  'simrat39/rust-tools.nvim',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-lua/plenary.nvim',
+    'mfussenegger/nvim-dap',
+  },
+  opts = {
+    server = {
+      on_attach = function(_, bufnr)
+        local rt = require("rust-tools")
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+      ['rust-analyzer'] = {
+        diagnostics = { enable = true },
+        inlayHints = {
+          typeHints = { enable = true },
+          expressionAdjustmentHints = { enable = "always" },
+          renderColons = { enable = true },
+          lifetimeElisionHints = {
+            enable = "always",
+            useParameterNames = true,
+            mode = "prefer_postfix"
+          },
+          check = {
+            command = "clippy"
+          },
+          checkOnSave = {
+            enable = true,
+            command = "clippy",
+          }
+        },
+      },
+      cmd = { 'rust-analyzer' },
+      filetypes = { 'rust' },
+    },
+  }
+}
